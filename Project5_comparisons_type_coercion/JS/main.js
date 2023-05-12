fb40191b490daf7bbbd948f4df2ea9e009473b5e
andreygalas <andreygalas2022@gmail.com>
2023-05-11 17:54:45 -0700

    PROJECT5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,24 @@
+// Use document.write() and typeof operator to display data type of a variable
+var myVar = "Hello World";
+document.write("Data type of 'myVar' is: " + typeof myVar + "<br>");
+
+// Write an expression combining a string and a number
+var myNum = 42;
+var myString = "The answer to everything is: ";
+document.write(myString + myNum + "<br>");
+
+// Use different operators to perform comparisons and logical operations
+var x = 10;
+var y = 5;
+
+document.write("x == y : " + (x == y) + "<br>"); // false
+document.write("x === y : " + (x === y) + "<br>"); // false
+document.write("x > y : " + (x > y) + "<br>"); // true
+document.write("x < y : " + (x < y) + "<br>"); // false
+document.write("x >= y : " + (x >= y) + "<br>"); // true
+document.write("x <= y : " + (x <= y) + "<br>"); // false
+document.write("x && y : " + (x && y) + "<br>"); // true
+document.write("x || y : " + (x || y) + "<br>"); // true
+document.write("!x : " + (!x) + "<br>"); // false
+
+// Add some comments explaining the code
